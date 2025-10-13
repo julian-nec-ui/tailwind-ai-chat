b3bed2a4635cbf9ecb8351978d52efa3d76767a0
jsn-qwerty <dev.eratos@gmail.com>
2025-10-13 13:45:14 -0500

    css changes

--- src/ai-model/AIChatModel.jsx
+++ src/ai-model/AIChatModel.jsx
@@ -95,16 +95,16 @@ export default function AIChatModel() {
 
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-[#21221ecd] via-[#245e7f] to-[#4c98a3] flex flex-col items-center justify-center p-4 gap-8">
-      <h1 className="text-6xl sm:text-7xl
-        bg-gradient-to-r from-[#e16767] via-[#e2f1f1] to-[#4add74]
-        bg-clip-text text-transparent text-center text-shadow-lg/20">
+    <div className="min-h-screen bg-gradient-to-br from-[#21221ecd] via-[#245e7f] to-[#54bfcf] flex flex-col items-center justify-center p-4 gap-8">
+      <h1 className="text-6xl sm:text-6xl
+        bg-gradient-to-r from-[#f80000] via-[#e2f1f1] to-[#4add74]
+        bg-clip-text text-transparent text-center text-shadow-lg/15">
         AI Chat With Models
       </h1>
 
-      <div className="flex flex-col sm:flex-row items-center gap-4">
-        <div className={`px-4 py-2 rounded-full text-sm
-        ${aiReady ? "bg-[#149b2689] text-white font-semibold border border-[#149b2689] item items-center gap-3 text"
+      <div className="flex flex-col sm:flex-row items-center gap-6">
+        <div className={`px-4 py-2 rounded-lg text-sm
+        ${aiReady ? "bg-gradient-to-r from-[#0c9692] to-[#0c4fae] border-none text-white font-semibold border item items-center gap-3 text"
             : "bg-[#17336b] text-[#ffffff] border border-[#17336b] font-semibold"}`}>
           {aiReady ? greenCheckbox.concat(" AI is Ready") : redXCheckbox.concat(" Waiting... AI not ready")}
         </div>
@@ -113,11 +113,11 @@ export default function AIChatModel() {
             Select a Chat Model:
           </span>
           <select value={selectedModel} onChange={handleModelChange} disabled={!aiReady}
-            className="bg-amber-600 border border-amber-400
+            className="bg-gradient-to-r from-[#c54675] to-[#6c476d] border-none
           rounded-lg px-3 py-2 text-white text-sm focus:outline-none
           disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer font-semibold">
             {models.map((model) => (
-              <option key={model.id} value={model.id} className="bg-amber-800 open:border-0">
+              <option key={model.id} value={model.id} className="bg-[#844560ee] open:border-0">
                 {model.name} ({model.provider})
               </option>
             ))}
@@ -125,23 +125,22 @@ export default function AIChatModel() {
         </div>
       </div>
 
-      <div className="w-full max-w-2xl 
-      bg-gradient-to-br from-orange-950/90 to-amber-950
-      backdrop:blur-md border border-orange-800/50 rounded-3xl p-6">
+      <div className="w-full max-w-2xl bg-[#17437095]
+      backdrop:blur-md border border-none rounded-3xl p-6">
         <div className="flex items-center justify-center
-        mb-4 p-2 bg-gradient-to-r from-orange-600/20 to-red-500/20
-        rounded-xl border border-orange-300/30">
-          <span className="text-orange-300 text-sm font-semibold">
+        mb-4 p-2 bg-gradient-to-r from-[#e88141] to-[#c12020]
+        rounded-xl border-none">
+          <span className="text-[#ffffff] text-sm font-semibold">
             {robot.concat(" Using chat model: ")} {currentModel.name} ({currentModel.provider})
           </span>
         </div>
         <div className="h-96 overflow-y-auto border-b
-        border-gray-600 mb-6 p-4 bg-gradient-to-b from-gray-900/50 to-gray-800/50 rounded-2xl">
+        border-none mb-6 p-4 bg-gradient-to-b from-[#333333] via-[#094886] to-[#111100] rounded-2xl">
           {messages.length === 0 && (
-            <div className="text-center text-gray-400 mt-20 text-xl">
+            <div className="text-center text-[#efe4e4] mt-20 text-xl">
               📢 Let's start a conversation. Type a message.
               <br />
-              <span className="text-xs text-gray-500 mt-2 block">
+              <span className="text-xs text-gray-400 mt-2 block">
                 You can try other listed models to check their behavior.
               </span>
             </div>
@@ -152,8 +151,8 @@ export default function AIChatModel() {
               <div key={msg.id} className={`
                 p-3 m-2 rounded-2xl w-fit max-w-[80%] text-wrap
                 ${
-                  msg.isUser ? "bg-gradient-to-r from-orange-500 to-red-500 text-white ml-auto text-right"
-                  : "bg-gradient-to-r from-amber-600 to-orange-600 text-white"
+                  msg.isUser ? "bg-gradient-to-r from-orange-600 to-red-400 text-white ml-auto text-right"
+                  : "bg-gradient-to-r from-amber-700 to-orange-600 text-white"
                 }`
               }>
                 <div className="whitespace-pre-wrap">
@@ -165,7 +164,7 @@ export default function AIChatModel() {
 
           {
             isLoading && (
-              <div className="p-3 m-2 rounded-2xl max-w-xs bg-gradient-to-r from-amber-600 to-red-600 text-white">
+              <div className="p-3 m-2 rounded-2xl max-w-xs bg-gradient-to-r from-red-600 to-amber-600 text-white">
                 <div className="flex items-center gap-2">
                   <div className="animate-spin w-4 h-4 border-2 border-white/30 border-t-white rounded-full"></div>
                     {currentModel.name} is working ...
@@ -182,14 +181,14 @@ export default function AIChatModel() {
                 : "Waiting for AI model to be ready..."
              }
             disabled={!aiReady || isLoading}
-            className="flex-1 px-4 py-3 bg-yellow-900/30 border border-amber-800 rounded-2xl
+            className="flex-1 px-4 py-3 bg-red-900/80 border-none rounded-2xl
             text-white placeholder-gray-400 focus:outline-none focus:ring-2
             focus:shadow-xl focus:shadow-amber-700/80
             focus:ring-orange-500 transition duration-400 disabled:opacity-50
             disabled:cursor-not-allowed" />
           <button onClick={sendMessage}
             disabled={!aiReady || isLoading || !inputValue.trim()}
-            className="px-6 py-3 bg-gradient-to-r from-orange-400 to-red-500 hover:opacity-80
+            className="px-6 py-3 bg-gradient-to-r from-red-900/80 to-red-500 hover:opacity-80
             text-white font-semibold rounded-2xl gap-1
             transition-all duration-300 disabled:opacity-50
             flex items-center justify-end
